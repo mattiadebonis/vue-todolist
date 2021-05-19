7193440129d9c528c05e37d50987df2ee07fd7a6
mattiadebonis <mattia.debonis@gmail.com>
2021-05-19 15:40:01 +0200

    css e vue-data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 var app = new Vue({
+    el: "#root",
+    data: {
+        toDo: [
+            {
+                nome: "Portare Ragù a spasso",
+                completato: false
+            },
+            {
+                nome: "Imbiancare ingresso",
+                completato: false
+            }
+        ]
+    },
+    methods:{
+
+    }
 
 
 });
